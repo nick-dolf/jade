f76daaed336fd6f786a4abd3e55bb9da03f23b59
nick-dolf <nickdolf@gmail.com>
2022-08-28 19:22:06 -0500

    setup template engine

--- jade.js
+++ jade.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const build = require('./admin/utils/build')
 require('dotenv').config()
+const marked = require('marked')
+marked.setOptions({ breaks: true })
+const path = require('path')
+const sanitizeHtml = require('sanitize-html')
 
 // Set up Different Environments
 if (process.env.NODE_ENV === 'development') {
@@ -12,6 +16,17 @@ if (process.env.NODE_ENV === 'development') {
   console.log("\n Staging environment")
 }
 
+// Set up Template Engine
+app.set('view engine', 'pug');
+app.set('views', path.join(process.cwd(), 'src/views'))
+app.locals.pretty = true;
+// Convert Markdown and sanitize the HTML
+app.locals.md = (data) => {
+  return sanitizeHtml(marked.parse(data))
+}
+
+
+// Build
 build.renderSass();
 
 // Set up routes
