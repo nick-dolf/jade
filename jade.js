0bfd7c3d99d2bea9c937916624b9d2eacfa2ea54
nick-dolf <nickdolf@gmail.com>
2022-09-07 12:15:48 -0500

    organized cms views

--- jade.js
+++ jade.js
@@ -9,11 +9,13 @@ marked.setOptions({ breaks: true })
 const path = require('path')
 const sanitizeHtml = require('sanitize-html')
 
-// Set up Different Environments
+// Set up Development Environment
 if (process.env.NODE_ENV === 'development') {
   console.log("\nDev environment")
+
   // On production this will be served by NGINX
   app.use(express.static('site'))
+ 
   // Live Reload
   const livereload = require('livereload')
   const liveReloadServer = livereload.createServer()
@@ -27,14 +29,14 @@ if (process.env.NODE_ENV === 'development') {
       liveReloadServer.refresh("/");
     }, 100);
   });
-} else if (process.env.NODE_ENV === 'staging') {
-  console.log("\n Staging environment")
-}
+
+  // Make html easy to read
+  app.locals.pretty = true;
+} 
 
 // Set up Template Engine
 app.set('view engine', 'pug');
 app.set('views', path.join(process.cwd(), 'src/views'))
-app.locals.pretty = true;
 // Convert Markdown and sanitize the HTML
 app.locals.md = (data) => {
   return sanitizeHtml(marked.parse(data))
